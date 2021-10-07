70994d03e58e76b6f61c30d5cf76bbf524c6e77b
Vladimir <vladimir@Designer-iMac.local>
2021-10-07 13:00:07 +0300

    fixex

--- scripts/index.js
+++ scripts/index.js
@@ -5,30 +5,36 @@ const profileName = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__form')
 const popupCloseButton = document.querySelector('.popup__close')
 const descriptionInput = document.querySelector('#input-description')
 const nameInput = document.querySelector('#input-name')
 const submitButton = document.querySelector('.popup__submit')
 
 
-// устанавливаем обработчики событий на кнопки
-profileEditButton.addEventListener('click', onEdit)
-popupCloseButton.addEventListener('click', onClose)
-submitButton.addEventListener('click', onSubmit)
 
 
 
 function onEdit() {
-    popup.classList.add('popup__opened')
+    nameInput.value = profileName.textContent
+    descriptionInput.value = profileSubtitle.textContent
+    popup.classList.add('popup_opened')
 }
 
 function onClose() {
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 }
 
 
-function onSubmit() {
+function onSubmit(event) {
+    event.preventDefault()
     profileName.textContent = nameInput.value
     profileSubtitle.textContent = descriptionInput.value
     onClose()
 }
+
+
+// устанавливаем обработчики событий на кнопки
+profileEditButton.addEventListener('click', onEdit)
+popupCloseButton.addEventListener('click', onClose)
+popupForm.addEventListener('submit', onSubmit)
